44ff1e89b9aae495575168bcd70b48e53df726a0
BaekYeonSun <20191604@kookmin.ac.kr>
2022-05-05 01:11:06 +0900

    Update frontend Notice API

---
+++ frontend/lib/models/notice_model.dart
@@ -0,0 +1,21 @@
+class Notice {
+  late String title;
+  late String content;
+
+  Notice({
+    required this.title,
+    required this.content,
+  });
+
+  Notice.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    content = json['content'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['content'] = this.content;
+    return data;
+  }
+}
