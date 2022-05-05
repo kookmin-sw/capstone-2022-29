8e09674fee87306fc72c41ad029b9fec4d95617c
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2022-05-05 19:56:37 +0900

    [FE/Refactor] Connect Search&TimeLinePage API

--- frontend/lib/components/news_title.dart
+++ frontend/lib/components/news_title.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 
-Widget newsTitle(Size size, String query) {
-  return Center(
-    child: Container(
-      height: size.height * 0.1,
-      width: size.width * 0.85,
-      margin: EdgeInsets.symmetric(
-        vertical: size.height * 0.01,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      alignment: Alignment.centerLeft,
-      padding: EdgeInsets.symmetric(
-        horizontal: size.width * 0.1,
-      ),
-      child: Text(
-        query,
-        style: TextStyle(
-          fontSize: size.height * 0.02,
+Widget newsTitle(Size size, String query, void Function() onTap) {
+  return InkWell(
+    onTap: onTap,
+    child: Center(
+      child: Container(
+        height: size.height * 0.1,
+        width: size.width * 0.85,
+        margin: EdgeInsets.symmetric(
+          vertical: size.height * 0.01,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(
+          horizontal: size.width * 0.1,
+        ),
+        child: Text(
+          query,
+          style: TextStyle(
+            fontSize: size.height * 0.02,
+          ),
         ),
       ),
     ),
