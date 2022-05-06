2b38efb4e5d0e3e72c1808d672976b65326d69ee
BaekYeonSun <20191604@kookmin.ac.kr>
2022-05-06 18:16:20 +0900

    Update Bookmark page Appbar & Search page Error

--- frontend/lib/components/app_bar.dart
+++ frontend/lib/components/app_bar.dart
@@ -3,38 +3,50 @@
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 
-AppBar appBar(Size size, String? title, BuildContext context, bool isBack, bool isShow) {
+AppBar appBar(
+    Size size, String? title, BuildContext context, bool isBack, bool isShow) {
   return AppBar(
     backgroundColor: Colors.transparent,
-    leading: isBack ? Container(
-      margin: EdgeInsets.only(left: size.width * 0.05),
-      child: IconButton(
-        icon: Image.asset('lib/assets/images/backIcon.png', width: size.width * 0.1, height: size.width * 0.1),
-        onPressed: () => Navigator.pop(context),
-      ),
-    ) : null,
-    title: title != ' ' ? Container(
-      child: Center( child: title != ''? Text(title ?? '',style: TextStyle(color: Colors.black)) : Image.asset('lib/assets/images/logo_mini.png')),
-      width: size.width * 0.4,
-      height: size.width * 0.1,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-      ),
-    ) : null,
+    leading: isBack
+        ? Container(
+            margin: EdgeInsets.only(left: size.width * 0.05),
+            child: IconButton(
+              icon: Image.asset('lib/assets/images/backIcon.png',
+                  width: size.width * 0.1, height: size.width * 0.1),
+              onPressed: () => Navigator.pop(context),
+            ),
+          )
+        : Container(),
+    title: title != ' '
+        ? Container(
+            child: Center(
+                child: title != ''
+                    ? Text(title ?? '', style: TextStyle(color: Colors.black))
+                    : Image.asset('lib/assets/images/logo_mini.png')),
+            width: size.width * 0.4,
+            height: size.width * 0.1,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+          )
+        : null,
     centerTitle: true,
     elevation: 0.0,
     actions: [
-      isShow? Container(
-        margin: EdgeInsets.only(right: size.width * 0.05),
-        child: IconButton(
-          icon: Icon(Icons.share_outlined),
-          color: Colors.black,
-          onPressed: () {
-            Share.share('Hello Welcome to FlutterCampus', subject: 'Welcome Message');
-          },
-        ),
-      ) : Container(),
+      isShow
+          ? Container(
+              margin: EdgeInsets.only(right: size.width * 0.05),
+              child: IconButton(
+                icon: Icon(Icons.share_outlined),
+                color: Colors.black,
+                onPressed: () {
+                  Share.share('Hello Welcome to FlutterCampus',
+                      subject: 'Welcome Message');
+                },
+              ),
+            )
+          : Container(),
     ],
   );
 }
