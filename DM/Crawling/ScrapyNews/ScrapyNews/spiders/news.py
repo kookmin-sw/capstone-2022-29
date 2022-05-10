f8647e9424f8fe6115a748a9796e8b59184846e1
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2022-05-11 02:10:28 +0900

    [DM/Scrapy] Crawling News article with Scrapy

--- DM/Crawling/ScrapyNews/ScrapyNews/spiders/news.py
+++ DM/Crawling/ScrapyNews/ScrapyNews/spiders/news.py
@@ -8,14 +8,8 @@ class NewsUrlSpider(scrapy.Spider):
     name = 'UrlCrawler'
 
     def start_requests(self):
-        urls = [
-            'https://www.donga.com/news/List?p=0&prod=news&ymd=&m=NP',
-        ]
+        return [scrapy.Request(url='https://www.donga.com/news/List?p={}&prod=news&ymd=&m=NP'.format(i), callback=self.parse) for i in range(0,200,20)]
 
-        return [scrapy.Request(url=url, callback=self.parse) for url in urls]
-        # 또는
-        #for url in urls
-        #    yield scrapy.Reqest(url=url, callback=self.parse
 
     def parse(self, response):
         for i in range(2,22):
@@ -25,11 +19,14 @@ class NewsUrlSpider(scrapy.Spider):
             title = response.xpath('//*[@id="content"]/div[{}]/div[@class="rightList"]/a/span[@class="tit"]/text()'.format(i)).extract_first()
             url = response.xpath('//*[@id="content"]/div[{}]/div[@class="rightList"]/a/@href'.format(i)).extract_first()            
 
+            article = Article(url)
+            article.download()
+            article.parse()
+
+            item['journal'] = '동아일보'
             item['date'] = datetime.fromisoformat(date).strftime("%Y-%m-%d")
             item['title'] = title
             item['url'] = url
-
+            item['content'] = article.text
 
             yield item
-
-
