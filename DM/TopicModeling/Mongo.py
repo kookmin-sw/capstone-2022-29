6a9472f202e1867d18a1da8fbe420c3a4923b7f1
HanJeong Lee <54920378+leehanjeong@users.noreply.github.com>
2022-05-14 23:08:21 +0900

    Delete Mongo.py

--- DM/TopicModeling/Mongo.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(host='127.0.0.1', port=27017, username='BaekYeonsun', password='hello12345')
-# db = clinet.database
-# collection = db.news
-print(client.list_database_names())
